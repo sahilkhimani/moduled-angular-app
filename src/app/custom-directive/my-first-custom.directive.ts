33dd166adab176a6f70e70695edbe0081baf94fb
Sahil Khimani <sahil.khimani@contour-software.com>
2024-12-24 13:56:17 +0500

    host binding

--- src/app/custom-directive/my-first-custom.directive.ts
+++ src/app/custom-directive/my-first-custom.directive.ts
@@ -1,17 +1,39 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit } from "@angular/core";
 
 @Directive({
     selector: '[myFirstCustomDirective]',
-    standalone : false
+    standalone: false
 })
-export class MyFirstCustomDirective implements OnInit{
-    private element : ElementRef;
-    constructor(elementRef : ElementRef){
+export class MyFirstCustomDirective implements OnInit {
+    private element: ElementRef;
+    constructor(elementRef: ElementRef) {
         this.element = elementRef;
     }
-    
-    ngOnInit(){
-        this.element.nativeElement.style.color = 'purple';
-        this.element.nativeElement.style.fontSize = '5rem';
+
+    @Input() defaultFont : string = '5rem';
+    @Input() defaultColor : string = 'purple';
+    @Input() hoverFont? : string;
+    @Input() hoverColor? : string;
+
+    @HostBinding('style.fontSize') fontSize: string = this.defaultFont;
+    @HostBinding('style.color') color: string = this.defaultColor;
+
+    ngOnInit() {
+        // this.element.nativeElement.style.color = 'purple';
+        // this.element.nativeElement.style.fontSize = '5rem';
     }
+
+    @HostListener('mouseenter') onMouseEnter(eventData: Event) {
+        //     this.element.nativeElement.style.fontSize = '10rem';
+        //     this.element.nativeElement.style.color = 'yellow';
+        this.fontSize = this.hoverFont? this.hoverFont : '10rem';
+        this.color = this.hoverColor? this.hoverColor : 'yellow';
+    }
+    @HostListener('mouseleave') onMouseLeave(eventData: Event) {
+    //     this.element.nativeElement.style.fontSize = '5rem';
+    //     this.element.nativeElement.style.color = 'purple';
+        this.fontSize = this.defaultFont;
+        this.color = this.defaultColor;
+    }
+
 }
--- src/app/custom-directive/second-custom.directive.ts
+++ src/app/custom-directive/second-custom.directive.ts
@@ -1,20 +1,28 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSecondCustomDirective]',
   standalone: false
 })
-export class SecondCustomDirective implements OnInit{
+export class SecondCustomDirective implements OnInit {
 
-  constructor(private elementRef : ElementRef, private renderer : Renderer2) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
   ngOnInit(): void {
-    
+
     const heading = this.renderer.createElement('h1');
     const text = this.renderer.createText('Hello from second custom directive');
     this.renderer.appendChild(heading, text);
-    
+
     const container = this.elementRef.nativeElement.querySelector('div#myCustomContainer');
     this.renderer.appendChild(container, heading);
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'blue');
+  }
+
+
+  @HostListener('mouseenter') onMouseEnter(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
+  }
+  @HostListener('mouseleave') onMouseLeave(data: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 }
